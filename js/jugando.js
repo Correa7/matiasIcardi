fee9d0855df5191997a3669ade82d3a8e6c28f1a
Correa7 <correadamian2019@gmail.com>
2023-01-21 23:41:03 -0300

    2

--- js/jugando.js
+++ js/jugando.js
@@ -9,6 +9,13 @@ piedra.addEventListener("click", eleccionJugador)
 papel.addEventListener("click", eleccionJugador)
 tijera.addEventListener("click", eleccionJugador)
 
+let resultado = document.getElementById("resultado")
+resultado.addEventListener("click",batalla)
+
+let jugador = ""
+let pc= ""
+let derrotas= ""
+let victorias=""
 function iniciaJuego(){
     let botonEleccionJugador = document.getElementById ("eleccion-jugador")
     // botonEleccionJugador.addEventListener("click", eleccionJugador)
@@ -17,66 +24,86 @@ function iniciaJuego(){
 
 function eleccionJugador (e){
   
-    console.log(e.target.value)
-    
-    if(e.target.value == "piedra"){
+    if(e.target.value == 1){
         Swal.fire ({
             title:"Elegiste `La buena Piedra` 🥌",
             showConfirmButton: false,
             timer: 1000
         })
-    } else if(e.target.value == "papel"){
-        Swal.fire ({title:"Elegiste Papel 🧻",
-        showConfirmButton: false,
-        timer: 1000
-    })
-    } else if(e.target.value == "tijera"){
-        Swal.fire ({title:"Elegiste Tijera ✂",
-        showConfirmButton: false,
-        timer: 1000
+    } else if(e.target.value == 2){
+        Swal.fire ({
+            title:"Elegiste Papel 🧻",
+            showConfirmButton: false,
+            timer: 1000
+        })
+    } else if(e.target.value == 3){
+        Swal.fire ({
+            title:"Elegiste Tijera ✂",
+            showConfirmButton: false,
+            timer: 1000
     })
     } else{
-        Swal.fire ("Recuerda seleccionar una opción")
+    Swal.fire ("Recuerda seleccionar una opción")
     }
     setTimeout(eleccionPc, 1500)
-  
+
+   jugador = e.target.value
 }
 
 function eleccionPc (){
     let aleatorioPc = aleatorio (1, 3)
-
     if (aleatorioPc == 1){
+       
         Swal.fire ({title:"PC seleccionó Piedra 🥌",
         showConfirmButton: false,
         timer: 1500
     })
     } else if (aleatorioPc == 2){
-        Swal.fire ({title:"PC seleccionó Papel 🧻",
-        showConfirmButton: false,
-        timer: 1500
+       
+        Swal.fire ({
+            title:"PC seleccionó Papel 🧻",
+            showConfirmButton: false,
+            timer: 1500
     })
     } else {
-        Swal.fire ({title:"PC seleccionó Tijera ✂",
-        showConfirmButton: false,
-        timer: 1500
+      
+        Swal.fire ({
+            title:"PC seleccionó Tijera ✂",
+            showConfirmButton: false,
+            timer: 1500
     })
-    }
+}
+    pc=aleatorioPc
+
+
 }
 
 function aleatorio(min, max){
     return Math.floor(Math.random()*(max-min+1)+1)
 }
 
-/* function batalla(jugador, pc){
+
+function batalla(){
+
     if(jugador == pc){
-        alert("Empate, juega de nuevo")
+        Swal.fire ({title:"Empate, juega de nuevo",
+        showConfirmButton: false,
+        timer: 1500
+    })
+        
     }else if((jugador == 1 && pc == 3) || (jugador == 2 && pc == 1) || (jugador == 3 && pc == 2)){
         victorias = victorias +1
-        alert("Ganaste, felicitaciones 👌")
+        Swal.fire ({title:"Ganaste, felicitaciones 👌",
+        showConfirmButton: false,
+        timer: 1500
+    })
+       
     }else{
         derrotas = derrotas + 1
-        alert("Perdiste, mejor suerte la próxima")
+        Swal.fire ({title:"Perdiste, mejor suerte la próxima",
+        showConfirmButton: false,
+        timer: 1500
+    })
     } 
-}
- */ 
 
+}
